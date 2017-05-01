fb528ce206bdc47591c69be4bd188962d671d538
Carl Weis <carl@givecampus.com>
2017-04-30 22:44:16 -0400

    Generate registrations controller for signing up

---
+++ test/controllers/registrations_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class RegistrationsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get signup_url
+    assert_response :success
+  end
+
+end
